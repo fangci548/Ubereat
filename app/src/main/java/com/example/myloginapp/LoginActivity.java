9fbefd6b1262b47fc52565405d89a9b4447a91b3
Your Name <you@example.com>
2022-05-17 23:02:15 +0800

    0517-K0-firebase for signin finish

--- app/src/main/java/com/example/myloginapp/LoginActivity.java
+++ app/src/main/java/com/example/myloginapp/LoginActivity.java
@@ -1,127 +1,60 @@
 package com.example.myloginapp;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.facebook.AccessToken;
-import com.facebook.CallbackManager;
-import com.facebook.FacebookCallback;
-import com.facebook.FacebookException;
-import com.facebook.login.LoginManager;
-import com.facebook.login.LoginResult;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.api.ApiException;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
-import java.util.Collections;
+public class LoginActivity extends AppCompatActivity
+        implements OnCompleteListener<AuthResult> {
 
-public class LoginActivity extends AppCompatActivity {
-
-    ImageView fbBtn;
-    CallbackManager callbackManager;
-    GoogleSignInOptions gso;
-    GoogleSignInClient gsc;
-    ImageView googleBtn;
+    private EditText etEmail;//d0813263@o365.fcu.edu.tw
+    private EditText etPassword;//abc1234
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
-        callbackManager = CallbackManager.Factory.create();
-        googleBtn = findViewById(R.id.google_btn);
-        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
-        gsc = GoogleSignIn.getClient(this,gso);
+        etEmail = findViewById(R.id.et_email);
+        etPassword = findViewById(R.id.et_password);
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
 
-        googleBtn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                signIn();
-            }
-        });
+    public void onLogin(View view) {
+        String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, this);
+    }
 
-        AccessToken accessToken = AccessToken.getCurrentAccessToken();
-        if(accessToken != null && !accessToken.isExpired()){
-            startActivity(new Intent(LoginActivity.this, FacebookActivity.class));
-            finish();
-        }
-
-        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
-        if(acct!=null){
-            navigateToThirdActivity();
-        }
-
-        LoginManager.getInstance().registerCallback(callbackManager,
-                new FacebookCallback<LoginResult>() {
-                    @Override
-                    public void onSuccess(LoginResult loginResult) {
-                        startActivity(new Intent(LoginActivity.this, FacebookActivity.class));
-                        finish();
-                    }
-
-                    @Override
-                    public void onCancel() {
-                        // App code
-                    }
-
-                    @Override
-                    public void onError(@NonNull FacebookException exception) {
-                        // App code
-                    }
-                });
-
-        fbBtn = findViewById(R.id.fb_btn);
-        fbBtn.setOnClickListener(view -> LoginManager.getInstance().logInWithReadPermissions(LoginActivity.this, Collections.singletonList("public_profile")));
-        TextView username = (TextView) findViewById(R.id.username);
-        TextView password = (TextView) findViewById(R.id.password);
-
-        Button login_btn = (Button) findViewById(R.id.button);
-
-        login_btn.setOnClickListener(view -> {
-            if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
-                //correct
-                Toast.makeText(LoginActivity.this,"LOGIN SUCCESSFUL",Toast.LENGTH_SHORT).show();
-            }else
-                //incorrect
-                Toast.makeText(LoginActivity.this,"LOGIN FAILED !!!",Toast.LENGTH_SHORT).show();
-        });
+    public void onRegister(View view) {
+        Intent intent = new Intent();
+        intent.setClass(this, RegisterActivity.class);
+        startActivity(intent);
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        callbackManager.onActivityResult(requestCode, resultCode, data);
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1000) {
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-
-            try {
-                task.getResult(ApiException.class);
-                navigateToThirdActivity();
-            } catch (ApiException e) {
-                Toast.makeText(getApplicationContext(),"Something went wrong", Toast.LENGTH_SHORT).show();
-            }
+    public void onComplete(@NonNull Task<AuthResult> task) {
+        if (task.isSuccessful()) {
+            Toast.makeText(this, "登入成功", Toast.LENGTH_LONG).show();
+            MainActivity.VALID_USER = true;
+            Intent intent = new Intent();
+            intent.setClass(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        } else {
+            Toast.makeText(this, "登入失敗", Toast.LENGTH_LONG).show();
         }
-    }
-
-    void signIn(){
-        Intent signInIntent = gsc.getSignInIntent();
-        startActivityForResult(signInIntent,1000);
-    }
-
-    void navigateToThirdActivity(){
-        finish();
-        Intent intent = new Intent(LoginActivity.this, GoogleActivity.class);
-        startActivity(intent);
     }
 }
