79896b5941414a7ad8e31dce2ca109f3c23dec36
FengTzu <fangci548@gmail.com>
2022-05-21 15:30:43 +0800

    0521-QQ

--- app/src/main/java/com/example/myloginapp/CustomItemList.java
+++ app/src/main/java/com/example/myloginapp/CustomItemList.java
@@ -2,16 +2,14 @@ package com.example.myloginapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 public class CustomItemList extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.custom_shop);
+        setContentView(R.layout.customer_shop);
     }
 
 //    public void jump(View view){
