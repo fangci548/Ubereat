d93021fb083c7e4965523d368ef41cb837acf235
D0845176 <D0845176@o365.fcu.edu.tw>
2022-05-27 00:52:18 +0800

    0527-chen-manager_manage_member,memberInfo,store_main,page_design

--- app/src/main/java/com/example/myloginapp/StoreProductActivity.java
+++ app/src/main/java/com/example/myloginapp/StoreProductActivity.java
@@ -1,14 +1,29 @@
 package com.example.myloginapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class StoreProductActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class StoreProductActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_store_product);
+  }
+
+  @Override
+  public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+    switch (item.getItemId()) {
+    }
+
+    return false;
   }
 }
