f79c724df1e158d65b54b75d48e974ebc02556fe
D0845176 <D0845176@o365.fcu.edu.tw>
2022-06-08 22:02:16 +0800

    0608-chen-store-orderdet_lv

--- app/src/main/java/com/example/myloginapp/StoreOrderCompleteDetActivity.java
+++ app/src/main/java/com/example/myloginapp/StoreOrderCompleteDetActivity.java
@@ -5,18 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class StoreOrderCompleteDetActivity extends AppCompatActivity {
 
   TextView mark;
   TextView num;
   TextView time;
+  ListView listView;
+  ArrayList<OrderDetailItem> arrayList = new ArrayList<>();
+  OrderDetItemAdapter orderdetitemAdapter;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_store_order_complete_det);
+
+    arrayList.add(new OrderDetailItem("1", "珍珠奶茶", "1", "60"));
+    arrayList.add(new OrderDetailItem("2", "鮪魚蛋餅", "1", "30"));
+    arrayList.add(new OrderDetailItem("3", "紅茶", "1", "20"));
+    listView = findViewById(R.id.orderdet_lv);
+    orderdetitemAdapter = new OrderDetItemAdapter(this,R.layout.store_orderdetail_layout, arrayList);
+    listView.setAdapter(orderdetitemAdapter);
 
     mark = findViewById(R.id.orderdet_mark);
     num = findViewById(R.id.orderdet_num);
