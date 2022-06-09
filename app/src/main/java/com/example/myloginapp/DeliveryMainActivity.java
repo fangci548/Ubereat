78babd777116272bac4db051a95d7304b1efe499
Your Name <you@example.com>
2022-06-09 18:59:25 +0800

    0609-K0-delivery account/navbar

--- app/src/main/java/com/example/myloginapp/DeliveryMainActivity.java
+++ app/src/main/java/com/example/myloginapp/DeliveryMainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -13,6 +14,8 @@ import java.util.ArrayList;
 public class DeliveryMainActivity extends AppCompatActivity {
 
     ListView listView;
+    public static final String ALBUM_NO = "album_no";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,19 +25,43 @@ public class DeliveryMainActivity extends AppCompatActivity {
         listView = findViewById(R.id.order_lv);
         ArrayList<DeliveryOrderItem> deliveryOrderList = new ArrayList<DeliveryOrderItem>();
 
-        deliveryOrderList.add(new DeliveryOrderItem("1","麥當勞","10.00","800"));
-        deliveryOrderList.add(new DeliveryOrderItem("2","KFC","25.00","1.8km"));
-        deliveryOrderList.add(new DeliveryOrderItem("3","必勝客","20.00","1km"));
+        deliveryOrderList.add(new DeliveryOrderItem("Q1032457","麥當勞","10.00","800m"));
+        deliveryOrderList.add(new DeliveryOrderItem("Q1073852","KFC","25.00","1.8km"));
+        deliveryOrderList.add(new DeliveryOrderItem("Q1032457","必勝客","20.00","1km"));
 
         DeliveryOrderAdapter adapter = new DeliveryOrderAdapter(this, R.layout.deliver_order_list, deliveryOrderList);
 
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Intent intent = new Intent();
+                intent.setClass(DeliveryMainActivity.this, DeliveryOrderConfirm.class);
+                intent.putExtra(ALBUM_NO, position);
+                startActivity(intent);
+
+            }
+        });
 
     }
 
     public void goToOrderConfirm(View v){
         Intent intent = new Intent();
         intent.setClass(this, DeliveryOrderConfirm.class);
+        startActivity(intent);
+    }
+
+    public void goToAccount(View v){
+        Intent intent = new Intent();
+        intent.setClass(this, DeliveryAccountActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToHome(View v){
+        Intent intent = new Intent();
+        intent.setClass(this, DeliveryMainActivity.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/myloginapp/DeliveryOrderConfirm.java
+++ app/src/main/java/com/example/myloginapp/DeliveryOrderConfirm.java
@@ -2,10 +2,15 @@ package com.example.myloginapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class DeliveryOrderConfirm extends AppCompatActivity {
+
+    private String name;
+    private String shopName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
